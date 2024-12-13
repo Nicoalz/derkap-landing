de9a0a35f60bdc07336062c05818ef386d73c6ea
Augustin Briolon <augustin.briolon@fortueno.com>
2024-12-13 11:42:19 +0100

    try to Extending the background into the iPhone Notch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e3426392dac50a2b"; }
+/******/ 		__webpack_require__.h = function() { return "f17f56d0f861bddf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
