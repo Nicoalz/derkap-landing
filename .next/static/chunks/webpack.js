4e532b90391585904b6691134380885a46021482
Augustin Briolon <augustin.briolon@fortueno.com>
2024-12-13 10:18:16 +0100

    Ajoute un support pour les zones de sécurité dans le CSS et supprime les fichiers de mise à jour Webpack obsolètes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6541764f2d50304"; }
+/******/ 		__webpack_require__.h = function() { return "891ec4e1ef43c15c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
