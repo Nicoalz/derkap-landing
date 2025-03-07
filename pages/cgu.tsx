5904a5321c47a248ac619e7f9ca801b64b26ce4e
Nico <nicobdx@icloud.com>
2025-03-07 13:35:15 +0100

    add cgu

---
+++ pages/cgu.tsx
@@ -0,0 +1,70 @@
+
+
+export default function Home() {
+  return (
+    <div className="section-container max-w-screen-2xl mx-auto h-screen flex items-center justify-center overflow-hidden">
+        <p>
+        1. Présentation de l&apos;Application {"\n"}
+                  Derkap est une application mobile permettant aux utilisateurs
+                  de créer et de participer à des challenges photo entre amis.
+                  Editeur : Derkap Siège social : Paris, France Contact :
+                  contact.derkap@gmail.com L&apos;utilisation de l&apos;application
+                  implique l&apos;acceptation sans réserve des présentes Conditions
+                  Générales d&apos;Utilisation (CGU). {"\n"} {"\n"}
+                  2. Accès et Utilisation de l&apos;Application {"\n"}
+                  L&apos;application est accessible uniquement aux utilisateurs âgés
+                  d&apos;au moins 16 ans. L&apos;utilisateur est seul responsable du
+                    contenu qu&apos;il publie et doit respecter les lois en vigueur.
+                  Les défis ne sont pas modérés par Derkap avant leur
+                  publication. Les utilisateurs s&apos;engagent à ne pas proposer ou
+                  réaliser des défis dangereux, illégaux, discriminatoires,
+                    violents ou portant atteinte à la dignité d&apos;autrui. {
+                    "\n"
+                  }{" "}
+                  {"\n"}
+                  3. Contenus Autorisés et Responsabilité {"\n"}
+                  Sont interdits : contenus à caractère violent, haineux,
+                  diffamatoire, discriminatoire, incitant à la haine ou portant
+                    atteinte à la vie privée. Aucune nudité ou contenu
+                  sexuellement explicite n&apos;est toléré. Derkap ne peut être tenu
+                  responsable des publications des utilisateurs. Un système de
+                  signalement est en place pour notifier les abus. Derkap se
+                  réserve le droit de supprimer un contenu ou de bannir un
+                  utilisateur ne respectant pas ces règles. {"\n"} {"\n"}
+                  4. Protection des Données Personnelles {"\n"}
+                  Données collectées : email, photos partagées dans les groupes.
+                  Les photos sont chiffrées pour garantir la vie privée des
+                  utilisateurs. Derkap ne vend ni ne partage les données des
+                  utilisateurs avec des tiers. L&apos;utilisateur peut demander la
+                  suppression de son compte et de ses données en envoyant un
+                  email à contact.derkap@gmail.com. Derkap ne peut être tenu
+                  responsable en cas de piratage ou de fuite de données. {
+                    "\n"
+                  }{" "}
+                  {"\n"}
+                  5. Monétisation {"\n"}
+                  Derkap ne contient ni publicité ni achats intégrés à sa
+                  création, mais se réserve le droit d&apos;en inclure à l&apos;avenir.{" "}
+                  {"\n"} {"\n"}
+                  6. Sanctions et Résiliation {"\n"}
+                  En cas de non-respect des règles, Derkap se réserve le droit
+                  de suspendre ou supprimer le compte de l&apos;utilisateur sans
+                  préavis. {"\n"} {"\n"}
+                  7. Responsabilités {"\n"}
+                  Derkap ne peut être tenu responsable des comportements abusifs
+                    des utilisateurs. L&apos;application est fournie &quot;en l&apos;état&quot; sans
+                  garantie de fonctionnement continu. Les utilisateurs sont
+                  responsables de leur propre utilisation et des conséquences de
+                  leurs publications. {"\n"} {"\n"}
+                  8. Modification des CGU Derkap {"\n"}
+                  se réserve le droit de modifier ces CGU à tout moment. Les
+                  utilisateurs en seront informés via l&apos;application. {"\n"}{" "}
+                  {"\n"}
+                  En utilisant Derkap, vous reconnaissez avoir lu et accepté ces
+                  CGU. En cas de non-respect, votre accès à l&apos;application pourra
+                  être restreint ou supprimé. {"\n"}
+                  Dernière mise à jour : 03/02/2025
+        </p>
+    </div>
+  );
+}
